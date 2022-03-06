ac8af1321030892e6c2f7ef252483d1015116a9b
sanketdesai <sanketdesai.162001@gmail.com>
2022-03-06 16:45:00 +0000

    This is my Homework for week5

---
+++ src/homeworkweek5/BioData.java
@@ -0,0 +1,15 @@
+package homeworkweek5;
+
+public class BioData {
+    public static void main(String [] args){
+
+        System.out.println("**BIO DATA**");
+        System.out.println("Name: Sanket");
+        System.out.println("Surname: Desai");
+        System.out.println("Gender: Male");
+        System.out.println("Age: 43");
+        System.out.println("Nationality: British");
+        System.out.println("Address: Harrow, Middlesex, UK");
+
+    }
+}
---
+++ src/homeworkweek5/DivideTwoNumber.java
@@ -0,0 +1,12 @@
+package homeworkweek5;
+
+public class DivideTwoNumber {
+    public static void main(String[] args) {
+
+        int x = 50;
+        int y = 3;
+
+        System.out.println("Test data: 50/3");
+        System.out.println("Output: " + (x / y));
+    }
+}
